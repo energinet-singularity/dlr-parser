4774a825aa5b26be1e93648e497d3978aa81f7ce
stigsberg <tsp@energinet.dk>
2021-12-17 15:03:33 +0100

    testing

--- tests/test_export_data.py
+++ tests/test_export_data.py
@@ -4,7 +4,7 @@
 # import pytest
 
 # Import modules that should be included for testing
-import dlr_limit_consumer
+import app.dlr_limit_consumer
 
 # Dummytest which will always succeed - must be replaced by real tests
 
