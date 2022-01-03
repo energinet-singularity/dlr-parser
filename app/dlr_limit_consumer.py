7507db8801a139a4ad97f1d882d43298c03b501b
stigsberg <tsp@energinet.dk>
2022-01-03 14:54:58 +0100

    created function instead of it included in main function on dlr_limit_consumer.py and added test of the format for the file the function creates

--- app/dlr_limit_consumer.py
+++ app/dlr_limit_consumer.py
@@ -4,28 +4,11 @@ import time
 import pandas as pd
 import csv
 import os
-
-
-IP = os.environ['KAFKA_IP']
-topic_name = os.environ['KAFKA_TOPIC']
-file_path = os.environ['FILE_PATH']
-file_name = os.environ['FILE_NAME']
+import sys
 
 # file_path = '/home/thomas/Desktop/workspace/python-client-kafka/kafka-scada-parser/kafka_to_csv.csv'
-
-cycle = 5
 show_debug = True
 show_data = True
-
-consumer = KafkaConsumer(
-    topic_name,
-    bootstrap_servers=[IP],
-    auto_offset_reset='latest',
-    group_id='Limit consumer',
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
-
-def add(a, b):
-    return a + b
 
 # Function to call the kafka consumer and taking the last element
 def consumer_kafka_to_csv():
@@ -36,9 +19,60 @@ def consumer_kafka_to_csv():
         consumer.commit()
         return message
 
+def export_to_file(file_name, data):
+    if len(data) != 0:
+        try:
+            with open(file_name, 'w+') as csv_file:
+                # List of variables to be used as keys for the excel file
+                title = ["I", "SEGLIM", "LINESEG_MRID", "LIMIT1", "LIMIT2", "LIMIT3"]
+                w = csv.DictWriter(csv_file, delimiter = ',', fieldnames = title)
+                w.writeheader()
+
+                # Extract list of keys into variable
+                dlr_keys = list(data[0].keys())
+                # We iterate through the rows in data and create a new row with the correct keys. Otherwise the csv won't accept the row for further processing
+                # Title[0] and Title[1] are static, while Title[2] through Title[5] are dynamically set from the keys used in the DLR data
+                for i in data:
+                    row = {title[0]: 'D', 
+                        title[1]: 'SEGLIM', 
+                        title[2]: i[dlr_keys[0]], 
+                        title[3]: i[dlr_keys[1]], 
+                        title[4]: i[dlr_keys[2]], 
+                        title[5]: i[dlr_keys[3]]}
+                    w.writerow(row)
+        except IOError:
+            print('I/O error')
 
 # Main loop
 if __name__ == "__main__":
+    # Input from user
+    IP = os.environ.get('KAFKA_IP')
+    topic_name = os.environ.get('KAFKA_TOPIC')
+    file_name = os.environ.get('FILE_NAME')
+
+    # Check to see if there is a input on the environment variables
+    if IP == "":
+        print('Input on IP is not set')
+        sys.exit(1)
+
+    if topic_name == "":
+        print('Input on topic_name is not set')
+        sys.exit(1)
+
+    if file_name == "":
+        print('Input on topic_name is not set')
+        sys.exit(1)
+
+    # Wait time used if kafka connection fails
+    cycle = 5
+
+    consumer = KafkaConsumer(
+        topic_name,
+        bootstrap_servers=[IP],
+        auto_offset_reset='latest',
+        group_id='Limit consumer',
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
+
     while True:
         # Start time
         start = time.time()
@@ -53,35 +87,7 @@ if __name__ == "__main__":
 
         if show_data: print(data)
 
-        # Ensures there is a file to write to at the target location
-        if not os.path.isfile(file_path):
-            df = pd.DataFrame(list())
-            df.to_csv(file_name)
-        
-        if len(data) == 0:
-            continue
-        else:
-            try:    
-                with open(file_path, 'w') as csv_file:
-                    # List of variables to be used as keys for the excel file
-                    title = ["I", "SEGLIM", "LINESEG_MRID", "LIMIT1", "LIMIT2", "LIMIT3"]
-                    w = csv.DictWriter(csv_file, delimiter = ',', fieldnames = title)
-                    w.writeheader()
-
-                    # Extract list of keys into variable
-                    dlr_keys = list(data[0].keys())
-                    # We iterate through the rows in data and create a new row with the correct keys. Otherwise the csv won't accept the row for further processing
-                    # Title[0] and Title[1] are static, while Title[2] through Title[5] are dynamically set from the keys used in the DLR data
-                    for i in data:
-                        row = {title[0]: 'D', 
-                            title[1]: 'SEGLIM', 
-                            title[2]: i[dlr_keys[0]], 
-                            title[3]: i[dlr_keys[1]], 
-                            title[4]: i[dlr_keys[2]], 
-                            title[5]: i[dlr_keys[3]]}
-                        w.writerow(row)
-            except IOError:
-                print('I/O error')
+        export_to_file(f'/data/{file_name}', data)
 
         end = time.time()
         if show_debug: print(f"Runtime of the program is {end - start}")
