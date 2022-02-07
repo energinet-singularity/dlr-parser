c6697c607bfa1f513afe98b90c9c435c74cf6aa8
Thomas Pedersen <91196174+stigsberg@users.noreply.github.com>
2022-02-07 08:43:02 +0100

    Add new documentation and minor code changes
    * added licens, new README.md and reconfigured function to parse kafka message to make it more widely useful
    * Changed test to include new variable
    * pylint corrections in dlr_limit_consumer.py
    * New python logging and updated the README.md
    * added enviroment variable to docker run command
    * changed warning level of Failed to connect to kafka in dlr_limit_consumer.py
    * changed handling of bool for shape_data

--- app/dlr_limit_consumer.py
+++ app/dlr_limit_consumer.py
@@ -1,92 +1,118 @@
 from kafka import KafkaConsumer
+from os import environ as env
 from json import loads
+import logging
 import time
 import csv
 import sys
-import os
 
-show_debug = True
-show_data = True
+# Initialize log
+log = logging.getLogger(__name__)
+
+# Input from user 
+try:
+    ip = env.get('KAFKA_IP')
+except KeyError:
+    log.warning('Input on KAFKA_IP is not set')
+    sys.exit(1)
+try:
+    topic_name = env.get('KAFKA_TOPIC')
+except KeyError:
+    log.warning('Input on KAFKA_TOPIC is not set')
+    sys.exit(1)
+try:
+    file_name = env.get('FILE_NAME')
+except KeyError:
+    log.warning('Input on FILE_NAME is not set')
+    sys.exit(1)
+
+# Parameter to disable shaping of the data and instead generate a file directly from the json list
+shape_data = env.get('SHAPE_DATA', 'True')
+shape_data = shape_data.upper() == 'TRUE'
 
 # Function to call the kafka consumer and taking the last element
 def consumer_kafka_to_csv():
-    # buffer = []
     for message in consumer:
-        print('Got message from kafka:')
-        print(message.value)
+        log.info('Got message from kafka')
         message = message.value
+        log.debug(message)
         consumer.commit()
         return message
 
-def export_to_file(file_name, data):
+def export_to_file(file_name, data, shape_data):
     if len(data) != 0:
-        try:
-            with open(file_name, 'w+') as csv_file:
-                # List of variables to be used as keys for the excel file
-                title = ["I", "SEGLIM", "LINESEG_MRID", "LIMIT1", "LIMIT2", "LIMIT3"]
-                w = csv.DictWriter(csv_file, delimiter = ',', fieldnames = title)
-                w.writeheader()
+        if shape_data:
+            try:
+                with open(file_name, 'w+') as csv_file:
+                    # List of variables to be used as keys for the excel file
+                    header = ["I", "SEGLIM", "LINESEG_MRID", "LIMIT1", "LIMIT2", "LIMIT3"]
+                    w = csv.DictWriter(csv_file, delimiter = ',', fieldnames = header)
+                    w.writeheader()
 
-                # Extract list of keys into variable
-                dlr_keys = list(data[0].keys())
-                # We iterate through the rows in data and create a new row with the correct keys. Otherwise the csv won't accept the row for further processing
-                # Title[0] and Title[1] are static, while Title[2] through Title[5] are dynamically set from the keys used in the DLR data
-                for i in data:
-                    row = {title[0]: 'D', 
-                        title[1]: 'SEGLIM', 
-                        title[2]: i[dlr_keys[0]], 
-                        title[3]: i[dlr_keys[1]], 
-                        title[4]: i[dlr_keys[2]], 
-                        title[5]: i[dlr_keys[3]]}
-                    w.writerow(row)
-        except IOError:
-            print('I/O error')
+                    # Extract list of keys into variable
+                    dlr_keys = list(data[0].keys())
+                    # We iterate through the rows in data and create a new row with the correct keys. Otherwise the csv won't accept the row for further processing
+                    # header[0] and header[1] are static, while header[2] through header[5] are dynamically set from the keys used in the DLR data
+                    for i in data:
+                        row = {header[0]: 'D',
+                               header[1]: 'SEGLIM',
+                               header[2]: i[dlr_keys[0]],
+                               header[3]: i[dlr_keys[1]],
+                               header[4]: i[dlr_keys[2]],
+                               header[5]: i[dlr_keys[3]]
+                               }
+                        w.writerow(row)
+            except IOError:
+                log.error('I/O error')
+        else:
+            try:
+                with open(file_name, 'w+', newline='') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    data_keys = list(data[0].keys())
+                    csv_writer.writerow(data_keys)
+
+                    for i in data:
+                        csv_writer.writerow(i.values())
+            except IOError:
+                log.error('I/O error')
+
 
 # Main loop
 if __name__ == "__main__":
-    # Input from user
-    IP = os.environ.get('KAFKA_IP')
-    topic_name = os.environ.get('KAFKA_TOPIC')
-    file_name = os.environ.get('FILE_NAME')
 
-    # Check to see if there is a input on the environment variables
-    if IP == "":
-        print('Input on IP is not set')
-        sys.exit(1)
-
-    if topic_name == "":
-        print('Input on topic_name is not set')
-        sys.exit(1)
-
-    if file_name == "":
-        print('Input on file_name is not set')
-        sys.exit(1)
+    # Setup logging for client output (__main__ should output INFO-level, everything else stays at WARNING)
+    logging.basicConfig(format="%(levelname)s:%(asctime)s:%(name)s - %(message)s")
+    logging.getLogger(__name__).setLevel(logging.INFO)
 
     # Wait time used if kafka connection fails
     cycle = 5
 
-    consumer = KafkaConsumer(
-        topic_name,
-        bootstrap_servers=[IP],
-        auto_offset_reset='latest',
-        group_id='Limit consumer',
-        value_deserializer=lambda x: loads(x.decode('utf-8')))
+    try:
+        consumer = KafkaConsumer(
+            topic_name,
+            bootstrap_servers=[ip],
+            auto_offset_reset='latest',
+            group_id='Limit consumer',
+            value_deserializer=lambda x: loads(x.decode('utf-8')))
+    except Exception:
+        log.exception('Connection to kafka failed. Check enviroment variable KAFKA_IP and reload the script/container to try again.')
 
     while True:
-        # Start time
-        start = time.time()
-        print('Starting loop')
+        
+        log.info('Starting loop')
         # Receiving the last message from the kafka topic
         try:
             data = consumer_kafka_to_csv()
         except Exception as e:
-            print(f"Failed to connect to kafka topic: {e}")
+            log.warning(f"Failed to connect to kafka topic: {e}")
             time.sleep(cycle)
             continue
+        
+        log.debug(data)
 
-        if show_data: print(data)
-
-        export_to_file(f'/data/{file_name}', data)
-
+        # Start time of the internal part of the program
+        start = time.time()
+        export_to_file(f'/data/{file_name}', data, shape_data)
         end = time.time()
-        if show_debug: print(f"Runtime of the program is {end - start}")
+
+        log.debug(f"Runtime of the program is {end - start}")
